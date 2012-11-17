8193089b4ab4b91887cc3660785e37a51e7a5ea3
whiskers75 <crazycats@yopmail.com>
2012-11-17 09:32:21 -0500

    Basic server software setup

---
+++ server.js
@@ -0,0 +1,24 @@
+// Zork Server by whiskers75
+
+var cp = require('child_process');
+var net = require('net');
+var readline = require('readline');
+var sessions = []
+var sockets = []
+
+
+var startZork = function(socket) {
+    sessions[sockets.indexOf(socket)] = cp.spawn('frotz Zork/DATA/ZORK1.DAT'); // Spawn Zork I using frotz
+    sessions[sockets.indexOf(socket)].stdout.pipe(socket);
+    sessions[sockets.indexOf(socket)].stderr.pipe(socket);
+    socket.pipe(sessions[sockets.indexOf(socket)].stdin);
+    sessions[sockets.indexOf(socket)].on('exit', function() {
+        socket.end();
+    });
+};
+
+
+net.createServer(function(socket) {
+    sockets.push(socket);
+    startZork(socket);   
+}).listen(5025);

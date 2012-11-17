2ba4f8dffa9732d54618787ab9c12f9bc28885db
whiskers75 <crazycats@yopmail.com>
2012-11-17 09:43:30 -0500

    Basic server software setup - test 5

--- server.js
+++ server.js
@@ -9,9 +9,12 @@ var sockets = []
 
 var startZork = function(socket) {
     sessions[sockets.indexOf(socket)] = cp.spawn('frotz '+process.env.PWD+'/Zork/DATA/ZORK1.DAT'); // Spawn Zork I using frotz
-    sessions[sockets.indexOf(socket)].stdout.pipe(socket);
-    sessions[sockets.indexOf(socket)].stderr.pipe(socket);
-    socket.pipe(sessions[sockets.indexOf(socket)].stdin);
+    socket.on('data', function(data) {
+        sessions[sockets.indexOf(socket)].stdin.write(data);
+    });
+    sessions[sockets.indexOf(socket)].stdout.on('data', function(data) {
+        socket.write(data);
+    });
     sessions[sockets.indexOf(socket)].on('exit', function() {
         socket.end();
     });

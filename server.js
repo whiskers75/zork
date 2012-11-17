8c87915a8a33672e4aaa0722004e9e617237ffd7
whiskers75 <crazycats@yopmail.com>
2012-11-17 10:02:18 -0500

    Basic server software setup - test 12

--- server.js
+++ server.js
@@ -3,16 +3,18 @@
 var cp = require('child_process');
 var net = require('net');
 var readline = require('readline');
-var sessions = []
-var sockets = []
+var sessions = [];
+var sockets = [];
+var readlines = []
 
 
 
 
 net.createServer(function(socket) {
     sockets.push(socket);
+    readlines[sockets.indexOf(socket)] = readline.createInterface(socket, socket);
     sessions[sockets.indexOf(socket)] = cp.spawn('./zork.sh'); // Spawn Zork I using frotz
-    socket.on('data', function(data) {
+    readlines[sockets.indexOf(socket)].on('line', function(data) {
         data = data.replace(/[\n\r]/g, '');
         if (data === '') {}
         else {
@@ -20,9 +22,10 @@ net.createServer(function(socket) {
         }
     });
     sessions[sockets.indexOf(socket)].stdout.on('data', function(data) {
-        socket.write(data);
+        readlines[sockets.indexOf(socket)].write(data);
     });
     sessions[sockets.indexOf(socket)].on('exit', function() {
+        readlines[sockets.indexOf(socket)].end();
         socket.end();
     });  
 }).listen(5025);

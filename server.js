7439cd742e4053cfa58e3a8a6ef3b116b330469c
whiskers75 <crazycats@yopmail.com>
2012-11-18 12:53:12 -0500

    fixed frotz hopefully 21

--- server.js
+++ server.js
@@ -80,7 +80,9 @@ net.createServer(function(socket) {
                                 sessions[sockets.indexOf(socket)].stdin.write(clients[sockets.indexOf(socket)].uid + '.sav\n');
                                 readlines[sockets.indexOf(socket)].on('line', function(data) {
                                     data = util.inspect(data);
-                                    data = data.replace(/[^A-Za-z]/g,"")
+                                    data = data.replace(/[^A-Za-z]/g,"");
+                                    data = data.substr(0, 'helprn'.length - 2)
+
                                     if (startsWith(data, 'restore')) {
                                         // You can't do that!
                                         socket.write('\n>');

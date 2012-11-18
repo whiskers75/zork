49237262461555055078e1603ec44acaaee10332
whiskers75 <crazycats@yopmail.com>
2012-11-18 12:59:16 -0500

    fixed frotz hopefully 24

--- server.js
+++ server.js
@@ -75,7 +75,8 @@ net.createServer(function(socket) {
                                 socket.write('Loading Zork...\n');
                                 sessions[sockets.indexOf(socket)] = cp.spawn('frotz', zorkargs);
                                 sessions[sockets.indexOf(socket)].stdout.setEncoding('utf8');
-                                sessions[sockets.indexOf(socket)].stdout.on('data', function(data) {socket.write(data)});
+                                sessions[sockets.indexOf(socket)].stdout.pipe(socket);
+                                sessions[sockets.indexOf(socket)].stderr.pipe(socket);
                                 sessions[sockets.indexOf(socket)].stdin.write('restore\n');
                                 sessions[sockets.indexOf(socket)].stdin.write(clients[sockets.indexOf(socket)].uid + '.sav\n');
                                 readlines[sockets.indexOf(socket)].on('line', function(data) {

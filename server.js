0dd101a9316e393030533bec16d3be72ccdf5958
whiskers75 <crazycats@yopmail.com>
2012-11-18 07:45:16 -0500

    Dropbox works! Fixing frotz 2

--- server.js
+++ server.js
@@ -70,7 +70,8 @@ net.createServer(function(socket) {
                                 socket.write('Savefile loaded.\n');
                                 readlines[sockets.indexOf(socket)] = readline.createInterface(socket, socket);
                                 socket.write('Loading Zork...\n');
-                                sessions[sockets.indexOf(socket)] = cp.spawn('./zork.sh'); // Spawn Zork I using frotz
+                                sessions[sockets.indexOf(socket)] = cp.spawn('./zork.sh');
+                                sessions[sockets.indexOf(socket)] = cp.spawn('zork');
                                 sessions[sockets.indexOf(socket)].stdout.on('data', function(data) {
                                     readlines[sockets.indexOf(socket)].write(data);
                                 });
